4459c99032f06ac3b9162edce7a04008fde2e5f1
Zaki Amin Ahmad <xakiamin122@gmail.com>
2026-05-12 13:55:25 -0400

    connected database

--- data-engine/main.py
+++ data-engine/main.py
@@ -1,6 +1,24 @@
 import time
-import schedule
+import threading
+import psycopg2
+import os
 from github_client import fetch_repo_metrics
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # github's s&p 500 lol
 TARGET_REPOS = [
@@ -11,11 +29,32 @@ TARGET_REPOS = [
     "fastapi/fastapi"
 ]
 
+live_market_data = []
+
+def save_to_db(ticker, price):
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+
+        query = """ INSERT INTO repositories (ticker, current_price, updated_at)
+                    VALUES (%s, %s, NOW())
+                    ON CONFLICT (ticker)
+                    DO UPDATE SET current_price = EXCLUDED.current_price, updated_at = NOW();
+                """
+        cur.execute(
+            query,
+            (ticker, price)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f"[DB ERROR] Could not save {ticker} to database: {e}")
+        return False
+
+
 def calculate_stock_price(metrics: dict) -> float:
-    """
-    The V1 Market Maker Algorithm.
-    Converts raw GitHub metrics into a simulated USD stock price.
-    """
     if not metrics:
         return 0.00
         
@@ -27,38 +66,43 @@ def calculate_stock_price(metrics: dict) -> float:
     star_value = metrics["stars"] * 0.01      # 100 stars = $1.00
     fork_value = metrics["forks"] * 0.05      # 20 forks = $1.00
     issue_penalty = metrics["open_issues"] * 0.02 
-    
     raw_price = star_value + fork_value - issue_penalty
     
     # checkfor stock NEVER going below 1 dollar to avoid penny stock status
     return max(round(raw_price, 2), 1.00)
 
-def market_tick():
-    """Runs one complete cycle of fetching data and updating prices."""
-    print("\n--- MARKET TICK (Fetching Live Data) ---")
-    
-    for repo in TARGET_REPOS:
-        metrics = fetch_repo_metrics(repo)
-        
-        if metrics:
-            price = calculate_stock_price(metrics)
-            print(f"[{metrics['ticker'].upper()}] : ${price:,.2f}") # =========== POST GRESSS ===========
-        time.sleep(1) # avoids github blocking so sleep 1s
-        
-    print("-------------------------------------------\n")
-
-if __name__ == "__main__":
-    print("Starting TRX Data Engine...")
-    
-    # Run it immediately once on startup
-    market_tick()
-    
-    # Schedule it to run every 1 minute
-    schedule.every(1).minutes.do(market_tick)
-    
-    print("Engine running. Waiting for next scheduled tick... (Press Ctrl+C to stop)")
-    
-    # Keep the script running forever
+def market_worker():
+    """Runs in the background, updating prices every 60 seconds."""
+    global live_market_data
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        print("\n[Data Engine] Fetching new market data...")
+        new_data = []
+        for repo in TARGET_REPOS:
+            metrics = fetch_repo_metrics(repo)
+            if metrics:
+                price = calculate_stock_price(metrics)
+                ticker = metrics["ticker"].upper()
+                sucess = save_to_db(ticker, price)
+                if sucess:
+                    print(f"synced {ticker} to Supabase: ${price}")
+                new_data.append({
+                    "ticker": metrics["ticker"].upper(),
+                    "price": price,
+                    "stars": metrics["stars"]
+                })
+            time.sleep(1) # Prevent GitHub rate limiting
+            
+        live_market_data = new_data
+        print("[Data Engine] Market data updated.")
+        time.sleep(60)
+
+# Start the background worker when the API starts
+@app.on_event("startup")
+def startup_event():
+    thread = threading.Thread(target=market_worker, daemon=True)
+    thread.start()
+
+# The endpoint our Next.js frontend will hit
+@app.get("/api/market")
+def get_market_data():
+    return {"market": live_market_data}
